33b073e1a11bc3ebbbf46105e662df76b314dda0
Dank Memetics <dankmemetics@yahoo.com>
2021-10-28 11:18:42 -0400

    UUPS Contract Upgrades (for DAO purposes)

--- solidity/contracts/DankFlair.sol
+++ solidity/contracts/DankFlair.sol
@@ -1,24 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
-contract DankFlair is ERC721, ERC721Burnable, Ownable {
-    using Counters for Counters.Counter;
+contract DankFlair is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    using CountersUpgradeable for CountersUpgradeable.Counter;
 
-    Counters.Counter private _tokenIdCounter;
+    CountersUpgradeable.Counter private _tokenIdCounter;
 
-    constructor() ERC721("Dank Flair", "DANK") {}
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
+    function initialize() initializer public {
+        __ERC721_init("DankFlair", "DANK");
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+    }
 
     function _baseURI() internal pure override returns (string memory) {
-        return "https://dankflair.cloud/ayo";
+        return "http://dankflair.cloud/42069";
     }
 
     function safeMint(address to) public onlyOwner {
         _safeMint(to, _tokenIdCounter.current());
         _tokenIdCounter.increment();
     }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
 }
---
+++ solidity/hardhat.config.js
@@ -0,0 +1,8 @@
+//  hardhat.config.js
+require('@nomiclabs/hardhat-ethers');
+require('@openzeppelin/hardhat-upgrades');
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.3",
+};
